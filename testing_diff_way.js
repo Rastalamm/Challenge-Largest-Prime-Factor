c2e622341be19dfb6d2819b0be7b237f407b2b8e
Judah Lamm <Judah.Lamm@gmail.com>
2015-05-29 00:02:41 -1000

    modified the spacing to match the style guide

--- testing_diff_way.js
+++ testing_diff_way.js
@@ -1,68 +1,70 @@
 exports.largestPrimeFactor = largestPrimeFactor;
-function largestPrimeFactor(n){
+function largestPrimeFactor(n) {
   var primeNumber = 1;
+
   // do your work here
-var start = Math.floor(n/2);
-while ( n != 1 ) {
-    if(n % start === 0){
-       primeNumber = start;
-       n = n / start;
-       //return start;
-    }else{
-       start--;
+  var start = Math.floor(n / 2);
+  while ( n != 1 ) {
+    if (n % start === 0) {
+      primeNumber = start;
+      n = n / start;
+
+    //return start;
+    }else {
+      start--;
     }
 
-}
-//while ends
-return primeNumber;
-};
+  }
 
+  //while ends
+  return primeNumber;
+}
 
 //Counting up
 
 exports.largestPrimeFactor = largestPrimeFactor;
-function largestPrimeFactor(n){
+function largestPrimeFactor(n) {
   var primeNumber = 1;
+
   // do your work here
-var start = 2;
-while ( n != 1 ) {
-    if(n % start === 0){
-       primeNumber = start;
-       n = n / start;
-    }else{
-       start++
+  var start = 2;
+  while ( n != 1 ) {
+    if (n % start === 0) {
+      primeNumber = start;
+      n = n / start;
+    }else {
+      start++;
     }
 
+  }
+
+  //while ends
+  return primeNumber;
 }
-//while ends
-return primeNumber;
-};
 
 //recursion
 //not working properly
-function findPF(n, cntr){
+function findPF(n, cntr) {
 
-    if(cntr === undefined){
-        cntr = 2;
+  if (cntr === undefined) {
+    cntr = 2;
+  }
+
+  if (n % cntr === 0) {
+    n = n / cntr;
+
+    if (n === 1) {
+      return n;
+    }else {
+      cntr++;
+      findPF(n, cntr);
     }
 
-    if(n % cntr === 0){
-        n = n/cntr;
-
-        if(n === 1){
-            return n;
-        }else{
-            cntr++;
-            findPF(n, cntr);
-        }
-
-    }else{
-        cntr++;
-         findPF(n, cntr);
-    }
-    return n;
-
+  }else {
+    cntr++;
+    findPF(n, cntr);
+  }
+  return n;
 }
-
 
 findPF(13195);
