4ca81d7e210d25deb188b2ddf5989b254670013a
Judah Lamm <Judah.Lamm@gmail.com>
2015-05-28 19:42:29 -1000

    Completion of the Largest Prime factor challenge

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,17 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
+exports.largestPrimeFactor = largestPrimeFactor;
+function largestPrimeFactor(n){
+  var primeNumber = 1;
   // do your work here
+var start = 2;
+while ( n != 1 ) {
+    if(n % start === 0){
+       primeNumber = start;
+       n = n /start;
+    }else{
+       start++
+    }
 
-  return primeNumber;
+}
+//while ends
+return primeNumber;
 };
